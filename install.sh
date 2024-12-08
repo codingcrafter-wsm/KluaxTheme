9cf3d0a9ed76a1aab72a3f3955b667cdd6ff2e8a
codingcrafter-wsm <138623449+codingcrafter-wsm@users.noreply.github.com>
2024-12-08 15:47:28 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,17 +12,17 @@ clear
 
 installTheme(){
     cd /var/www/
-    tar -cvf IceMinecraftTheme.tar.gz pterodactyl
+    tar -cvf KluaxTheme.tar.gz pterodactyl
     echo "Installing theme..."
     cd /var/www/pterodactyl
-    rm -r IceMinecraftTheme
-    git clone https://github.com/Angelillo15/IceMinecraftTheme.git
-    cd IceMinecraftTheme
-    rm /var/www/pterodactyl/resources/scripts/IceMinecraftTheme.css
+    rm -r KluaxTheme
+    git clone https://github.com/codingcrafter-wsm/KluaxTheme.git
+    cd KluaxTheme
+    rm /var/www/pterodactyl/resources/scripts/KluaxTheme.css
     rm /var/www/pterodactyl/resources/scripts/index.tsx
     rm /var/www/pterodactyl/resources/scripts/components/server/console/Console.tsx
     mv resources/scripts/index.tsx /var/www/pterodactyl/resources/scripts/index.tsx
-    mv resources/scripts/IceMinecraftTheme.css /var/www/pterodactyl/resources/scripts/IceMinecraftTheme.css
+    mv resources/scripts/IceMinecraftTheme.css /var/www/pterodactyl/resources/scripts/KluaxTheme.css
     mv resources/scripts/components/server/console/Console.tsx /var/www/pterodactyl/resources/scripts/components/server/console/Console.tsx
     cd /var/www/pterodactyl
 
@@ -60,39 +60,21 @@ installThemeQuestion(){
 }
 
 repair(){
-    bash <(curl https://raw.githubusercontent.com/Angelillo15/IceMinecraftTheme/main/repair.sh)
+    bash <(curl https://raw.githubusercontent.com/codingcrafter-wsm/KluaxTheme/main/repair.sh)
 }
 
 restoreBackUp(){
     echo "Restoring backup..."
     cd /var/www/
-    tar -xvf IceMinecraftTheme.tar.gz
-    rm IceMinecraftTheme.tar.gz
+    tar -xvf KluaxTheme.tar.gz
+    rm KluaxTheme.tar.gz
 
     cd /var/www/pterodactyl
     yarn build:production
     sudo php artisan optimize:clear
 }
                                                                                                                            
-printf "${blue} ____       _____        ______          _________________  ____   ____      ______        ______  _______        ______    \n"
-printf "${blue}|    |  ___|\    \   ___|\     \        /                 \|    | |    | ___|\     \      |      \/       \   ___|\     \   \n"
-printf "${blue}|    | /    /\    \ |     \     \       \______     ______/|    | |    ||     \     \    /          /\     \ |     \     \  \n"
-printf "${blue}|    ||    |  |    ||     ,_____/|         \( /    /  )/   |    |_|    ||     ,_____/|  /     /\   / /\     ||     ,_____/| \n"
-printf "${blue}|    ||    |  |____||     \--'\_|/          ' |   |   '    |    .-.    ||     \--'\_|/ /     /\ \_/ / /    /||     \--'\_|/ \n"
-printf "${blue}|    ||    |   ____ |     /___/|              |   |        |    | |    ||     /___/|  |     |  \|_|/ /    / ||     /___/|   \n"
-printf "${blue}|    ||    |  |    ||     \____|\            /   //        |    | |    ||     \____|\ |     |       |    |  ||     \____|\  \n"
-printf "${blue}|____||\ ___\/    /||____ '     /|          /___//         |____| |____||____ '     /||\____\       |____|  /|____ '     /| \n"
-printf "${blue}|    || |   /____/ ||    /_____/ |         |    |          |    | |    ||    /_____/ || |    |      |    | / |    /_____/ | \n"
-printf "${blue}|____| \|___|    | /|____|     | /         |____|          |____| |____||____|     | / \|____|      |____|/  |____|     | / \n"
-printf "${blue}  \(     \( |____|/   \( |_____|/            \(              \(     )/    \( |_____|/     \(          )/       \( |_____|/  \n"
-printf "${blue}   '      '   )/       '    )/                '               '     '      '    )/         '          '         '    )/     \n"
-printf "${blue}              '             '                                                   '                                    '      \n"
-echo ""
-echo "Copyright (c) 2022 Angelillo15 | angelillo15.es"
-echo "This program is free software: you can redistribute it and/or modify"
-echo ""
-echo "Discord: https://discord.angelillo15.es/"
-echo "Website: https://angelillo15.es/"
+printf "${blue}KluaxTheme\n"
 echo ""
 echo "[1] Install theme"
 echo "[2] Restore backup"
